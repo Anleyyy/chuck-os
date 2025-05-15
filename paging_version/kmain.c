56c44e854fd6c24848830d96b65beb10147d9d60
Gohu2000 <hugo.barzu@gmail.com>
2025-05-15 03:44:09 +0200

    Update kmain.c

--- paging_version/kmain.c
+++ paging_version/kmain.c
@@ -4,11 +4,18 @@
 
 extern unsigned int kernel_page_directory[1024];
 extern unsigned int white[1];
+char pages[NB_PAGES];
+
+void change_page(unsigned char value, unsigned int i) {
+    pages[i] = value;
+}
+
+char get_page(unsigned int i) {
+    return pages[i];
+}
 
 void unicorn(unsigned char fg, unsigned char bg)
 {
     clear_console();
-    struct output hello_world = {50,"Hello world~ Let's goooo !!",fg,bg};
-    print_str(hello_world);
-    print_addr(200, kernel_page_directory);
+    init_pages();
 }
