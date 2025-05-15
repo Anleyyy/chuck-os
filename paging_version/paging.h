b6223b67b8002db44face5263240405321efaf1f
Gohu2000 <hugo.barzu@gmail.com>
2025-05-15 03:42:27 +0200

    Update paging.h

--- paging_version/paging.h
+++ paging_version/paging.h
@@ -1,5 +1,5 @@
 #define END_KERNEL 0x00400000
-#define NB_PAGES_KERNEL 0x00000400
+#define NB_PAGES_KERNEL 0x00000200
 #define NB_PAGES 0x00080000
 #define MEM_SIZE 0x80000000
 #define PAGE_SIZE 0x00001000
@@ -22,3 +22,8 @@ void vmm_switch(unsigned int* new_page_directory);
 void* phys_alloc_page();
 
 unsigned int* vmm_create_pd(); 
+
+void vmm_map(unsigned int* pd, virtual_address_t va, unsigned int flags);
+
+
+void map_alias(unsigned int* page_directory, unsigned int virt1, unsigned int virt2, unsigned int flags);
